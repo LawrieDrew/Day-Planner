5ee718d36fe3a398e9404a3655dfe79250c73464
LawrieDrew <laurenn.drew@gmail.com>
2021-03-06 16:17:42 -0500

    setting up date and hourly display to reflect present day. setting up function to change timeblocks depending if the hour is past, present, or in the future

---
+++ script.js
@@ -0,0 +1,24 @@
+//setting planner date to current 
+var todayDisplay = moment().format('dddd, MMMM Do YYYY');
+$('currentDay').text(todayDisplay)
+
+//setting hour to current 
+var currentHour = moment().format('HH');
+console.log(currentHour);
+
+//now change color based on the hours past present and future
+changeColor();
+//if the current hour is less than the value of the timeblock, it will grey out
+//if the current hour is more than the value of the timeblock, it will blue out
+//if the current hour is equal to the value of the timeblock, it will yellow out
+if (currentHour < timeBlock){
+$()
+}
+if (currentHour > timeBlock ){
+
+}
+if (currentHour === timeBlock){
+
+}
+
+
