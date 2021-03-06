99bc56490a6899b1a8368fd0d6f3283d09d8bb82
LawrieDrew <laurenn.drew@gmail.com>
2021-03-06 17:16:36 -0500

    code is running without errorsgit add .! date is showing, hours are showing. code seems to be working but colors are not changing just yet.

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 //setting planner date to current 
 var todayDisplay = moment().format("dddd, MMMM Do YYYY");
-$("#currentDay").text(todayDisplay)
+$("#currentDay").text(todayDisplay);
 
+
+//now change color of rows based on the hours past present and future
+function changeColor() {
+    $(".timeValue").each(function(index,item){
 //setting hour to current 
-var currentHour = moment().format('HH');
+var currentHour = moment().format("HH");
 console.log(currentHour);
-
-//now change color based on the hours past present and future
-changeColor();
+var currentInt = parseInt(currentHour);
+var timeBlock = parseInt($(item).attr("data-hour"));
 //if the current hour is less than the value of the timeblock, it will grey out
 //if the current hour is more than the value of the timeblock, it will blue out
 //if the current hour is equal to the value of the timeblock, it will yellow out
-if (currentHour < timeBlock){
-$()
+if (currentInt < timeBlock){
+    $(item).addClass("future");
 }
-if (currentHour > timeBlock ){
-
+else if (currentInt > timeBlock){
+    $(item).addClass("past");
 }
-if (currentHour === timeBlock){
+else {
+    $(item).addClass("present");
+    }
+})};
 
-}
+changeColor();
 
-
+//retain text information in timeblock by saving to local storage
+//window.localStorage.setItem("textArea", JSON.stringify(userName) );
